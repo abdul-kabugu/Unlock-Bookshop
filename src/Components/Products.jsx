6372042cb1d2eda36a55f4962fa1d3c3a76a990e
ABDUL-KABUGU <kabuguabdul2@gmail.comL>
2022-02-21 10:13:36 +0300

    adding feedback alert

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,16 +1,31 @@
 import { FileSearchOutlined,DownloadOutlined } from '@ant-design/icons/lib/icons';
-import React from 'react';
-import { Button, Typography } from 'antd';
+import React,{useState} from 'react';
+import { Alert, Button, Typography } from 'antd';
 import { Books } from '../Books';
 const { Title, Text} = Typography
 
 console.log(Books)
 const Products = ({payWallStatus}) => {
+    const [isShowModal, setShowModal] = useState(false)
+    const  showModal = () => {
+        isShowModal === false? setShowModal(true) : setShowModal(false)
+        console.log("show modal clicked   " + isShowModal)
+    }
     console.log("this  is is paywall from product" +  " " + payWallStatus)
     return <div className=' container products-container'>
-    
-       <Title>trending  books</Title> 
+       {isShowModal && <Alert 
+          message="Success"
+      description="This  Is  Demo No  Real Books 
+      If You Like This Demo Let Me know On twitter / unlock  Discord server
+      "
+      type="success"
+      showIcon
+       
+       
+       />}
+       <Title>Trending  books</Title> 
        <div className='book-card-container'>
+    
        {Books.map(book =>(
        
            <div className='book-card' key={book.id}>
@@ -18,7 +33,7 @@ const Products = ({payWallStatus}) => {
                <p className='card-title'>{book.title}</p>
                {//payWallStatus === 'unlock' && book.isFree !== true?  <button className='card-btn'> <DownloadOutlined  style={{color: "white", fontSize: "20px",}}/> donload </button>:
                //<button className='card-btn'> <DownloadOutlined  style={{color: "white", fontSize: "20px",}}/> buy NFT</button>
-                payWallStatus === 'unlocked'?  <button className='card-btn'> <DownloadOutlined  style={{color: "white", fontSize: "20px",}}/> donload </button>:
+                payWallStatus === 'unlocked'?  <button className='card-btn' onClick={showModal}> <DownloadOutlined  style={{color: "white", fontSize: "20px",}}/> donload </button>:
                 <button className='card-btn' onClick={() => window.unlockProtocol && window.unlockProtocol.loadCheckoutModal()}> <DownloadOutlined  style={{color: "white", fontSize: "20px",}}/> buy NFT</button>
  
                }
